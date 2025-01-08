46601e4ce282c085e7c3df6a252e0116445ddb3d
MohammedJasim001 <jasimat001@gmail.com>
2025-01-08 19:06:01 +0530

    added dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes, useLocation} from 'react-router-dom';
 import LoginCard from './pages/auth/login';
+import Sidebar from './components/Common/Sidebar';
+import Home from './pages/HomePage';
+import Dashboard from './pages/Admin/AdminDashboard';
 
 function App() {
   const location=useLocation()
@@ -11,11 +14,14 @@ function App() {
   return (
     <>
     <div>
-      <p>
-        home page
-      </p>
+      <div>
+      {!noSidebarPaths.includes(location.pathname) && <Sidebar />}
+      </div>
     <Routes>
+      <Route path='/' element = {<Home/>}/>
       <Route path='auth/login' element={<LoginCard/>}/>
+      <Route path='/admin/dashboard' element= {<Dashboard/>}/>
+      
     </Routes>
     </div>
     </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 
 const Home = () => {
   return (
-    <div>HomePage</div>
+    <div >HomePage</div>
   )
 }
 
